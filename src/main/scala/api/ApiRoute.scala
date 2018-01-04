8ae39788aeeed1da9fd9c22b7926da8f2c9092fb
CassieAin <roksolana.d22@gmail.com>
2018-01-04 10:32:44 +0200

    New route to API added

--- src/main/scala/api/ApiRoute.scala
+++ src/main/scala/api/ApiRoute.scala
@@ -3,15 +3,18 @@ package api
 import akka.http.scaladsl.server.Directives._
 import db.Databases
 
-trait ApiRoute extends Databases{
-    val routes = pathSingleSlash {
-        complete("hello")
-    } ~
-      pathPrefix("tweets") {
-        pathSingleSlash {
+trait ApiRoute extends Databases {
+  val routes = pathPrefix("tweets") {
+    pathSingleSlash {
+      complete("hello")
+    }
+  } ~
+    pathPrefix("tweets") {
+      pathPrefix(LongNumber) {
+        userId =>
           get {
-              complete("")
-            }
+            complete("tweets by user")
           }
-        }
+      }
+    }
 }
