120bea4770ab63f7e71cb2566e3664a710935d0b
CassieAin <roksolana.d22@gmail.com>
2018-01-08 01:27:35 +0200

    Prepared source for insertion to db

--- src/main/scala/stream/Counter.scala
+++ src/main/scala/stream/Counter.scala
@@ -6,10 +6,12 @@ import akka.stream._
 import akka.stream.scaladsl._
 import akka.{Done, NotUsed}
 import db.Databases
+import models._
 import twitter4j.{Status, StatusAdapter}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
+import slick.jdbc.PostgresProfile.api._
 
 class Counter extends StatusAdapter with Databases{
   implicit val system = ActorSystem("TweetsExtractor")
@@ -24,11 +26,18 @@ class Counter extends StatusAdapter with Databases{
     bufferSize,
     overflowStrategy
   )
+  
+  val createTweetTable = TweetTable.table.schema.create
+  val createAuthorTable = AuthorTable.table.schema.create
+  val createLocationTable = LocationTable.table.schema.create
 
   val sink: Sink[Any, Future[Done]] = Sink.foreach(println)
   val flow: Flow[Status, String, NotUsed] = Flow[Status].map(status => status.getText)
   val graph = statusSource via flow to sink
   val queue = graph.run()
+
+//  val insertSink = Source[Status] foreach tweetRepository.create(_)
+//  val insertGraph = statusSource runWith insertSink
 
   override def onStatus(status: Status) = {
     Await.result(queue.offer(status), Duration.Inf)
