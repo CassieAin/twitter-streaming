e72c3918083102923fdc52821217d75b5fd59c70
CassieAin <roksolana.d22@gmail.com>
2018-01-10 20:00:24 +0200

    Removed unused repositories

--- src/main/scala/db/Databases.scala
+++ src/main/scala/db/Databases.scala
@@ -6,6 +6,4 @@ import slick.jdbc.PostgresProfile.api._
 trait Databases {
   val db = Database.forConfig("scalaxdb")
   val tweetRepository = new TweetRepository(db)
-  val authorRepository = new AuthorRepository(db)
-  val locationRepository = new LocationRepository(db)
 }
