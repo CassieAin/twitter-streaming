8d179c84d5f3a65826b8080f3b313e261c656b3a
CassieAin <roksolana.d22@gmail.com>
2018-01-10 17:21:26 +0200

    Location added to tweet

--- src/main/scala/models/Tweet.scala
+++ src/main/scala/models/Tweet.scala
@@ -1,19 +1,20 @@
 package models
 import slick.jdbc.PostgresProfile.api._
+
 import scala.concurrent.Future
 
-case class Tweet(tweetId: Long, author: Long, text: String, language: String, favoriteCount: Int, retweetCount: Int)
-//location: String,
+case class Tweet(tweetId: Long, author: Long, text: String, language: String, favoriteCount: Int, retweetCount: Int
+                , location: String)
 class TweetTable(tag: Tag) extends Table[Tweet](tag, "tweets"){
   val tweetId = column[Long]("tweetId", O.PrimaryKey, O.AutoInc)
   val author = column[Long]("author")
   val text = column[String]("text")
   val language = column[String]("language")
-//  val location = column[String]("location")
+  val location = column[String]("location")
   val favoriteCount = column[Int]("favoriteCount")
   val retweetCount = column[Int]("retweetCount")
 
-  def * = (tweetId, author, text, language, favoriteCount, retweetCount) <> (Tweet.apply _ tupled, Tweet.unapply)
+  def * = (tweetId, author, text, language, favoriteCount, retweetCount, location) <> (Tweet.apply _ tupled, Tweet.unapply)
 }
 
 object TweetTable{
